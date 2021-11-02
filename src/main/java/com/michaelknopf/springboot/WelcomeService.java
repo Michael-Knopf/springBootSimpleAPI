35a95684ec53b495361282ae58a8e326b23355b7
Michael_Knopf <mikeknopf2@gmail.com>
2021-11-02 14:19:49 -0400

    add dynamic configuration

--- src/main/java/com/michaelknopf/springboot/WelcomeService.java
+++ src/main/java/com/michaelknopf/springboot/WelcomeService.java
@@ -1,10 +1,15 @@
 package com.michaelknopf.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WelcomeService {
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
 	public String retrieveWelcomeMessage() {
-		return "Good Morning Michael!";
+		return welcomeMessage;
 	}
 }
