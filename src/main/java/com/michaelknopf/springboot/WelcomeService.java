1bce17c9f7cab1d856217a7b5157a882a753996f
Michael_Knopf <mikeknopf2@gmail.com>
2021-11-02 12:21:19 -0400

    add spring boot dev tools

--- src/main/java/com/michaelknopf/springboot/WelcomeService.java
+++ src/main/java/com/michaelknopf/springboot/WelcomeService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class WelcomeService {
 	public String retrieveWelcomeMessage() {
-		return "Good Morning Michael";
+		return "Good Morning Michael!";
 	}
 }
